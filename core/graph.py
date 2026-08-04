d214c658366b5ffc77a7d81d43fbbf552d0f6d4a
rare-sight <nikhilpanchal2724@gmail.com>
2026-08-04 17:30:54 +0530

    starts with entity

--- core/graph.py
+++ core/graph.py
@@ -1,24 +1,29 @@
 from langgraph.graph import StateGraph, START, END
 from core.state import InferaState
-from core.nodes import (
-    analyze_topic,
-    research_topic,
-    identify_uncertainties,
-    generate_scenarios,
-)
+
+from core.nodes.entity_node import entity_node
+from core.nodes.research import research_topic
+from core.nodes.reconcile_node import reconcile_node
+from core.nodes.analyze import analyze_topic
+from core.nodes.uncertainties import identify_uncertainties
+from core.nodes.scenarios import generate_scenarios
 
 
 def build_infera_graph():
     builder = StateGraph(InferaState)
 
-    builder.add_node("analyze", analyze_topic)
+    builder.add_node("entity", entity_node)
     builder.add_node("research", research_topic)
+    builder.add_node("reconcile", reconcile_node)
+    builder.add_node("analyze", analyze_topic)
     builder.add_node("uncertainties", identify_uncertainties)
     builder.add_node("scenarios", generate_scenarios)
 
-    builder.add_edge(START, "analyze")
-    builder.add_edge("analyze", "research")
-    builder.add_edge("research", "uncertainties")
+    builder.add_edge(START, "entity")
+    builder.add_edge("entity", "research")
+    builder.add_edge("research", "reconcile")
+    builder.add_edge("reconcile", "analyze")
+    builder.add_edge("analyze", "uncertainties")
     builder.add_edge("uncertainties", "scenarios")
     builder.add_edge("scenarios", END)
 
